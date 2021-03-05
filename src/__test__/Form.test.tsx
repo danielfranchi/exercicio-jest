a3e8c57484a38fc9769c50adbd9fd0f50cb6cb44
danielfranchi <damiel2mf@yahoo.com.br>
2021-03-05 16:21:35 -0300

    'exercício-jest'

--- src/__test__/Form.test.tsx
+++ src/__test__/Form.test.tsx
@@ -5,43 +5,47 @@ import userEvent from "@testing-library/user-event";
 describe("testing form", () => {
   it("testing the name form", async () => {
     render(<Form />);
-    const form = screen.getByTitle(/form/i)
-    expect(form).toBeTruthy()
+    const form = screen.getByTitle(/form/i);
+    expect(form).toBeTruthy();
 
-    const name = screen.getByRole('textbox', {
-      name: /name/i
-    })
-    expect(name).toBeTruthy()
-    userEvent.type(name, "daniel")
+    const name = screen.getByRole("textbox", {
+      name: /name/i,
+    });
+    expect(name).toBeTruthy();
+    userEvent.type(name, "daniel");
+    fireEvent.submit(form);
 
-    const cidade = screen.getByRole('textbox', {
-      name: /cidade/i
-    })
-    expect(cidade).toBeTruthy()
-    userEvent.type(cidade, "campinas")
+    const cidade = screen.getByRole("textbox", {
+      name: /cidade/i,
+    });
+    expect(cidade).toBeTruthy();
+    userEvent.type(cidade, "campinas");
+    fireEvent.submit(form);
 
-    const mail = screen.getByRole('textbox', {
-      name: /mail/i
-    })
-    expect(mail).toBeTruthy()
-    userEvent.type(mail, "daniel@live.com")
+    const mail = screen.getByRole("textbox", {
+      name: /mail/i,
+    });
+    expect(mail).toBeTruthy();
+    userEvent.type(mail, "daniel@live.com");
+    fireEvent.submit(form);
 
-    const idade = screen.getByRole('spinbutton', {
-      name: /idade/i
-    })
-    expect(idade).toBeTruthy()
-    const age = '20'
-    userEvent.type(idade, age)
+    const idade = screen.getByRole("spinbutton", {
+      name: /idade/i,
+    });
+    expect(idade).toBeTruthy();
+    const age = "20";
+    userEvent.type(idade, age);
+    fireEvent.submit(form);
 
-    const checkbox = screen.getByRole('checkbox', {
-      name: /estadocivil/i
-    })
-    expect(checkbox).toBeTruthy()
+    const checkbox = screen.getByRole("checkbox", {
+      name: /estadocivil/i,
+    });
+    expect(checkbox).toBeTruthy();
+    fireEvent.submit(form);
 
-    const btn = screen.getByRole('button', {
-      name: /enviar/i
-    })
-    expect(btn).toBeTruthy()
-
+    const btn = await screen.findByRole("button", {
+      name: /enviar/i,
+    });
+    expect(btn).toBeTruthy();
   });
 });
