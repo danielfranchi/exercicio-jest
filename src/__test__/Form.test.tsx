12bf6d8e53c3a9c7a97cc38ad059dce5ba8ae45e
danielfranchi <damiel2mf@yahoo.com.br>
2021-03-05 15:24:14 -0300

    'exercício-jest'

--- src/__test__/Form.test.tsx
+++ src/__test__/Form.test.tsx
@@ -6,31 +6,37 @@ describe("testing form", () => {
   it("testing the name form", async () => {
     render(<Form />);
     const form = screen.getByTitle(/form/i)
+    expect(form).toBeTruthy()
 
     const name = screen.getByRole('textbox', {
       name: /name/i
     })
+    expect(name).toBeTruthy()
     userEvent.type(name, "daniel")
 
     const cidade = screen.getByRole('textbox', {
       name: /cidade/i
     })
+    expect(cidade).toBeTruthy()
     userEvent.type(cidade, "campinas")
 
     const mail = screen.getByRole('textbox', {
       name: /mail/i
     })
+    expect(mail).toBeTruthy()
     userEvent.type(mail, "daniel@live.com")
 
     const idade = screen.getByRole('spinbutton', {
       name: /idade/i
     })
+    expect(idade).toBeTruthy()
     const age = '20'
     userEvent.type(idade, age)
 
-    screen.getByRole('checkbox', {
+    const checkbox = screen.getByRole('checkbox', {
       name: /estadocivil/i
     })
+    expect(checkbox).toBeTruthy()
 
     const btn = screen.getByRole('button', {
       name: /enviar/i
